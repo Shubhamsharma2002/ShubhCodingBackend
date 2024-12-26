15ef7ad27982cc6af012734c826410b3323da160
shubham sharma <shubhamtech004@gmail.com>
2024-12-27 02:06:07 +0530

    login , signup api done here

--- src/Controller/UserController.js
+++ src/Controller/UserController.js
@@ -1,7 +1,51 @@
 import {User} from "../Models/User.Model.js"
 import bcrypt from "bcryptjs";
-const register = async(req,res)=>{}
-const login = async(req,res)=>{}
+import jwt from "jsonwebtoken"
+const secret = "secret";
+const register = async(req,res)=>{
+    let { username, name, email, password } = req.body;
+  let emailCon = await User.findOne({ email: email });
+  if (emailCon) {
+    return res.json({ success: false, message: "Email already exists" });
+  }
+  else {
+
+    bcrypt.genSalt(10, function (err, salt) {
+      bcrypt.hash(password, salt, function (err, hash) {
+        let user = User.create({
+          username: username,
+          name: name,
+          email: email,
+          password: hash
+        });
+
+        return res.json({ success: true, message: "User created successfully" });
+      });
+    });
+
+  }
+}
+const login = async(req,res)=>{
+    let { email, password } = req.body;
+  let user = await User.findOne({ email: email });
+
+  if (user) {
+    // Rename the second `res` to avoid conflict
+    bcrypt.compare(password, user.password, function (err, isMatch) {
+      if (err) {
+        return res.json({ success: false, message: "An error occurred", error: err });
+      }
+      if (isMatch) {
+        let token = jwt.sign({ email: user.email, userId: user._id }, secret);
+        return res.json({ success: true, message: "User logged in successfully", token: token, userId: user._id });
+      } else {
+        return res.json({ success: false, message: "Invalid email or password" });
+      }
+    });
+  } else {
+    return res.json({ success: false, message: "User not found!" });
+  }
+}
 
 
 export {register,login};
