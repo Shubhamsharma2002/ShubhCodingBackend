f716d031dba7c75c0c8d0cc2191462835dbf02a7
shubham sharma <shubhamtech004@gmail.com>
2024-12-27 02:46:16 +0530

    handling the cors policy

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-
+import cors from'cors';
 import express from "express";
 import router from "./src/Routes/index.js";
 
 
 
 const server = express();
+server.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    Credential:true
+}));
 
 server.use(
     express.urlencoded({ extended: true })
