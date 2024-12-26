85a6f5b0207b50ee74ec15dd9d3474cee82bafc1
shubham sharma <shubhamtech004@gmail.com>
2024-12-27 01:47:34 +0530

    router and controller setup done for authenctiaction

---
+++ src/Controller/UserController.js
@@ -0,0 +1,7 @@
+import {User} from "../Models/User.Model.js"
+import bcrypt from "bcryptjs";
+const register = async(req,res)=>{}
+const login = async(req,res)=>{}
+
+
+export {register,login};
--- src/Routes/auth/AuthRouter.js
+++ src/Routes/auth/AuthRouter.js
@@ -1,5 +1,13 @@
 import express from "express";
+import { login, register } from "../../Controller/UserController.js";
 const userRouter = express.Router();
 
-
+userRouter.route('/registaer').post(
+    
+    register
+)
+userRouter.route('/login').post(
+   
+    login
+)
 export default userRouter;
